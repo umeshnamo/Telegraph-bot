eb75e428dec39ce0cfb5d9d8a03004e31f6b404c
umeshnamo <65161113+umeshnamo@users.noreply.github.com>
2021-06-04 14:54:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,12 @@ tgraph = Client(
 )
 
 
-
+@tgraph.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text(
+        text=f"Hello {message.from_user.mention},\nI'm a telegram to telegra.ph image uploader bot by @umeshnamo",
+        disable_web_page_preview=True
+    )
 
 
 @tgraph.on_message(filters.photo)
